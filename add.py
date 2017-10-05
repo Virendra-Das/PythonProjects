9d226fba8a3091f88f86eebcfa4850ce7d441ccf
Virendra Prajapati <virendraprajapatiji@gmail.com>
2017-10-05 17:52:03 +0530

    Add files via upload
    First work of me

---
+++ add.py
@@ -0,0 +1,11 @@
+num1= input("Enter first number")
+num2= input("Enter second number")
+res = int(num1) + int(num2)
+print("Sum is %s" % res)
+
+print("Type 2")
+
+num1= int(input("Enter first number"))
+num2= input("Enter second number")
+res = num1 + int(num2)
+print("Sum is %s" % res)
---
+++ test.py
@@ -0,0 +1,3 @@
+name = input("What is your name?").lower()
+count = 1
+print("Hello %s"% name)
