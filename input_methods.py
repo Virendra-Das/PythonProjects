3fc6f59a5555810faeef6180d26da29c36b46ca2
Virendra Prajapati <virendra.prajapati@CALSOFTHQ.COM>
2017-10-09 11:08:12 +0530

    xml file. a py file for i/o tests

---
+++ input_methods.py
@@ -0,0 +1,12 @@
+name = input("Enter your name please : ")
+print("So you are %s"%name)
+
+name = input("Enter name again : ")
+print("Wecome again Mr. "+name)
+
+name = input("Just one more time : ")
+print("Welcome ",name)
+print("Observe extra space due to , before name")
+
+name = input("The last test input. Enter name : ")
+print("Wecome again Mr. {0}".format(name))
